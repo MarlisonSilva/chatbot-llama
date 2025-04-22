e871bc49dbfd65353cbc7302fe447ba6d9836069
MarlisonSilva <marlisonsoaresjp@gmail.com>
2025-04-22 18:13:36 -0300

    fix: melhorias na segmentação dos dados e no prompt

--- question.py
+++ question.py
@@ -6,27 +6,28 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
 # Carregar JSON
-with open("regulamento_dos_cursos_de_graduacao_ufrn_data.json", "r", encoding="utf-8") as f:
+with open("data.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 print("CARREGADO")
 
 # Extrair e segmentar documentos
-documents = []
-for page in data["pages"]:
-    for item in page["items"]:
-        if item["type"] in ["heading", "text"]:
-            documents.append({
-                "content": item["md"],
-                "metadata": {
-                    "page": page["page"],
-                    "type": item["type"],
-                    "value": item["value"]
-                }
-            })
+# documents = []
+# for page in data["pages"]:
+#     for item in page["items"]:
+#         if item["type"] in ["heading", "text"]:
+#             documents.append({
+#                 "content": item["md"],
+#                 "metadata": {
+#                     "page": page["page"],
+#                     "type": item["type"],
+#                     "value": item["value"]
+#                 }
+#             })
+documents = data
 print("EXTRAIDO E SEGMENTADO")
 
 # Gerar embeddings
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("multi-qa-mpnet-base-dot-v1")
 contents = [doc["content"] for doc in documents]
 embeddings = model.encode(contents, show_progress_bar=True)
 embeddings = np.array(embeddings).astype("float32")  # Conversão necessária para o FAISS
@@ -55,7 +56,7 @@ llama_model = llama_model.to(device)
 print(f"USANDO DISPOSITIVO: {device}")
 
 # Função de recuperação
-def retrieve_documents(query, top_k=3):
+def retrieve_documents(query, top_k=5):
     query_embedding = model.encode([query])
     query_embedding = np.array(query_embedding).astype("float32")
     distances, indices = index.search(query_embedding, top_k)
@@ -65,7 +66,7 @@ def retrieve_documents(query, top_k=3):
 # Função de geração
 def generate_response(query, retrieved_docs):
     context = "\n\n".join([doc["content"] for doc in retrieved_docs])
-    prompt = f"Contexto:\n{context}\n\nPergunta: {query}\n\nResposta:"
+    prompt = f"Contexto extraído do regulamento da UFRN (incluindo artigos e definições): \n{context}\n\n Com base exclusivamente nesse conteúdo, responda claramente à pergunta abaixo, citando o artigo se possível. \n\nPergunta: {query}\n\nResposta:"
     print(prompt)
     inputs = tokenizer(prompt, return_tensors="pt").to(device)
     outputs = llama_model.generate(**inputs, max_new_tokens=300)
